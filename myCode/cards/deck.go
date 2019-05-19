de151ad3ea08654ff04f0979bbf511297a9f07bf
hparpia8 <hparpia8@gmail.com>
2019-05-19 19:51:59 -0230

    Added my learning code

---
+++ myCode/cards/deck.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// Create a new tryp of 'deck'
+// which is a slice of a strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// d is a convention for go which represents the object of deck
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+
+}
